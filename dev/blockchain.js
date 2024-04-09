f981be8e006fa8f3f384eeef5962e9cc050c06d7
Sakhawath Hossen Sohan <11808582+localhost443@users.noreply.github.com>
2024-04-09 15:22:18 +0600

    added creating new block functionalities

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -3,4 +3,18 @@ class BlockChain {
         this.chain = [];
         this.newTransactions = [];
     }
+
+    createNewBlock(nonce, previousBlockHash, hash) {
+        const newBlock = {
+            index: this.chain.length + 1,
+            time: Date.now(),
+            transactions: this.newTransactions,
+            nonce, hash, previousBlockHash
+        }
+
+        this.newTransactions = []
+        this.chain.push(newBlock)
+    }
+
+
 }
